d0c73aa3efbfab1891abcbd71049e19a3aaf7653
gautokr <kiki1060211@hotmail.com>
2024-10-03 11:31:36 -0400

    modified

--- 3-funciones.js
+++ 3-funciones.js
@@ -2,17 +2,17 @@
 
 // 1. Tu primera función
 // 1.1 Escribe una función llamada `despedir` que imprima "Adiós" en la consola.
-function mensajeDespedir(){
+function despedir(){
     console.log("Adiós");
 }
-mensajeDespedir();
+despedir();
 
 // 1.2 Crea una función llamada `multiplicarPorDos` que tome un número como parámetro y devuelva el doble de ese número.
 function multiplicarPorDos(numero){
-    let resultado = numero * 2;
-    return resultado;
+    let result = numero * 2;
+    console.log(result);
 }
-console.log(multiplicarPorDos(25));
+multiplicarPorDos(25);
 
 // 1.3 Crea una función llamada `esMayorDeEdad` que tome un número como parámetro y devuelva `true` si es mayor de 18, o `false` en caso contrario.
 function esMayorDeEdad(numero){
@@ -27,10 +27,11 @@ esMayorDeEdad(20);
 
 // 2. Parámetros
 // 2.1 Crea una función llamada `multiplicar` que reciba dos parámetros, los multiplique y devuelva el resultado.
-function nameMultiplicar(a, b){
-    return a * b;
+function multiplicar(a, b){
+    let result = a * b;
+    console.log(result);
 }
-console.log(nameMultiplicar(3, 6));
+multiplicar(3, 6);
 
 // 2.2 Escribe una función llamada `saludarPersonalizado` que reciba dos parámetros: `nombre` y `apellido`. La función debe devolver "Hola, [nombre] [apellido]".
 function saludarPersonalizado(nombre, apellido){
@@ -40,28 +41,32 @@ saludarPersonalizado('Juan ', 'Gómez');
 
 // 2.3 Crea una función llamada `calcularPotencia` que reciba dos parámetros: base y exponente. La función debe devolver el resultado de elevar la base al exponente.
 function calcularPotencia(base, exponente){
-    return base ** exponente;
+    let result = base ** exponente;
+    console.log(result);
 }
-console.log(calcularPotencia(5, 2));
+calcularPotencia(5, 2);
 
 // 2.4 Escribe una función llamada `restar` que reciba dos parámetros y devuelva la diferencia entre ellos.
-function nombreRestar(a, b){
-    return a - b;
+function restar(a, b){
+    let result = a - b;
+    console.log(result);
 }
-console.log(nombreRestar(15, 10));
+restar(15, 10);
 
 // 2.5 Crea una función llamada `dividir` que reciba dos parámetros y devuelva el resultado de la división del primer parámetro por el segundo.
-function nombreDividir(a, b){
-    return a / b;
+function dividir(a, b){
+    let result = a / b;
+    console.log(result);
 }
-console.log(nombreDividir(15, 5));
+dividir(15, 5);
 
 // 3. Function Expression
 // 3.1 Crea una **function expression** que multiplique dos números y asígnala a la variable `multiplicar`.
 let multiplicar = function(a, b){
-    return a * b;
+    let result = a * b;
+    console.log(result);
 }
-console.log(multiplicar(2, 3));
+multiplicar(2, 3); 
     
 // 3.2 Escribe una **function expression** llamada `saludar` que acepte un parámetro `nombre` y devuelva "Hola, [nombre]".
 let saludar = function(nombre){
@@ -85,8 +90,11 @@ esPar(10);
 // function multiplicar(a, b) {
 //     return a * b;
 // }
-const multiplicarFlecha = (a, b) => a * b;
-    console.log(multiplicarFlecha(3, 4));
+const multiplicarFlecha = (a, b) => {
+    let result = a * b;
+    console.log(result);
+}
+multiplicarFlecha(4, 3);
 
 // 4.2 Crea una función flecha llamada `saludarFlecha` que reciba un parámetro `nombre` y devuelva "Hola [nombre]".
 const saludarFlecha = (nombre) => {
@@ -96,9 +104,10 @@ saludarFlecha('Karen');
 
 // 4.3 Escribe una función flecha llamada `calcularArea` que reciba el radio de un círculo y devuelva el área del círculo. Usa la fórmula A = π * r².
 const calcularArea = radio => {
-    return Math.PI * radio * radio;
+    let result = Math.PI * radio * radio;
+    console.log(result);
 }
-console.log(calcularArea(10))
+calcularArea(10);
 
 // 5. Recursividad
 // 5.1 Escribe una función que calcule la suma de los primeros n números enteros de forma recursiva. Por ejemplo: `suma(3) -> 1 + 2 + 3 = 6`
@@ -106,9 +115,11 @@ function suma(n) {
     if (n === 0){
         return 0;
     }
-    return n + suma (n-1);
+    let result = n + suma (n-1);
+    console.log(result);
+    return result;
 }
-console.log(suma(4));
+suma(4);
 
 // 5.2 Escribe una función que calcule la sucesión de Fibonacci de forma recursiva. La sucesión de Fibonacci es una serie de números que empieza por 0 y 1 y cada número es la suma de los dos anteriores. Por ejemplo: `fibonacci(6) -> 8`
 function fibonacci(n) {
@@ -118,9 +129,11 @@ function fibonacci(n) {
     if (n===1){
         return 1;
     }
-    return fibonacci(n - 1) + fibonacci (n - 2);
+    let result = fibonacci(n - 1) + fibonacci (n - 2);
+    console.log(result);
+    return result;
 }
-console.log(fibonacci(4));
+fibonacci(4);
 
 // 5.3 Crea una función recursiva llamada `factorial` que calcule el factorial de un número. El factorial de n (n!) es el producto de todos los números enteros positivos menores o iguales a n.
 function factorial(n) {
@@ -130,9 +143,11 @@ function factorial(n) {
     if (n===1){
         return 1;
     }
-    return n * factorial(n-1);
+    let result = n * factorial(n - 1);
+    console.log(result);
+    return result;
 }
-console.log(factorial(6));
+factorial(6);
 
 // 5.4 Escribe una función recursiva que calcule la potencia de un número dado el exponente. Por ejemplo: `potencia(2, 3) -> 8`
 function potencia(base, exponente) {
@@ -142,20 +157,22 @@ function potencia(base, exponente) {
     if (exponente===1){
         return base;
     }
-    return base * potencia(base, exponente - 1);
+    let result = base * potencia(base, exponente - 1);
+    console.log(result);
+    return result;
 }
-console.log(potencia(3, 3));
+potencia(3, 3);
 
 // Exportar las funciones para que puedan ser probadas
 module.exports = {
-    mensajeDespedir,
+    despedir,
     multiplicarPorDos,
     esMayorDeEdad,
     multiplicar,
     saludarPersonalizado,
     calcularPotencia,
-    nombreRestar,
-    nombreDividir,
+    restar,
+    dividir,
     saludar,
     esPar,
     calcularArea,
